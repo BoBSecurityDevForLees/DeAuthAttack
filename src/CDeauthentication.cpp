d243a46525467d33d5d030dc8f43f0f6206b13d1
arad4228 <arad4228@gmail.com>
2023-01-26 21:25:41 +0900

    [KyoungJae]Fix CDeauthentication class

--- src/CDeauthentication.cpp
+++ src/CDeauthentication.cpp
@@ -1,23 +1,23 @@
-#include <CDeauthentication.h>
+#include "CDeauthentication.h"
 
-C80211Deauthentication::C80211Deauthentication(char* strBSSID)
+C80211Deauthentication::C80211Deauthentication(char* byteBSSID)
 {
     this->framecontrol = 0x00c0;
     this->duration = 0;
-    char strBroadCast[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-    memcpy(&this->strDestinationAddress, strBroadCast, 6);
-    memcpy(&this->strSourceAddress, strBSSID, 6);
-    memcpy(&this->strBSSID,strBSSID, 6);
+    BYTE byteBroadCast[6] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+    memcpy(&this->byteDestinationAddress, byteBroadCast, 6);
+    memcpy(&this->byteSourceAddress, byteBSSID, 6);
+    memcpy(&this->byteBSSID, byteBSSID, 6);
     this->byteFixed = 0x0070;
 }
 
-C80211Deauthentication::C80211Deauthentication(char* strBSSID, char* strDestination)
+C80211Deauthentication::C80211Deauthentication(char* byteBSSID, char* byteDestination)
 {
     this->framecontrol = 0x00c0;
     this->duration = 0;
-    memcpy(&this->strDestinationAddress, strDestination, 6);
-    memcpy(&this->strSourceAddress, strBSSID, 6);
-    memcpy(&this->strBSSID,strBSSID, 6);
+    memcpy(&this->byteDestinationAddress, byteDestination, 6);
+    memcpy(&this->byteSourceAddress, byteBSSID, 6);
+    memcpy(&this->byteBSSID, byteBSSID, 6);
 }
 
 C80211Deauthentication::~C80211Deauthentication()
@@ -29,14 +29,13 @@ bool C80211Deauthentication::getDeauthenticationPacket(char* packet)
 {
     memcpy(&packet[0], &this->framecontrol, 2);
     memcpy(&packet[2], &this->duration, 2);
-    memcpy(&packet[4], &this->strDestinationAddress, 6);
-    memcpy(&packet[10], &this->strSourceAddress, 6);
-    memcpy(&packet[16], &this->strBSSID, 6);
+    memcpy(&packet[4], &this->byteDestinationAddress, 6);
+    memcpy(&packet[10], &this->byteSourceAddress, 6);
+    memcpy(&packet[16], &this->byteBSSID, 6);
     uint16_t sequenceFrame = this->sequenceNum << 4;
     memcpy(&packet[22], &sequenceFrame, 2);
     memcpy(&packet[24], &this->byteFixed, 2);
     this->sequenceNum = (this->sequenceNum +1) % 16777216;
-
     return true;
 }
 
--- src/CDeauthentication.h
+++ src/CDeauthentication.h
@@ -3,20 +3,22 @@
 #include <iostream>
 #include <cstring>
 
+typedef u_char BYTE;
+
 class C80211Deauthentication
 {
 private:
     uint16_t framecontrol;
     uint16_t duration;
-    u_char strDestinationAddress[6];
-    u_char strSourceAddress[6];
-    u_char strBSSID[6];
+    BYTE byteDestinationAddress[6];
+    BYTE byteSourceAddress[6];
+    BYTE byteBSSID[6];
     uint sequenceNum = 0;
     uint16_t byteFixed;
 
 public:
-    C80211Deauthentication(char* strBSSID);
-    C80211Deauthentication(char* strBSSID, char* strDestination);
+    C80211Deauthentication(char* byteBSSID);
+    C80211Deauthentication(char* byteBSSID, char* byteDestination);
     ~C80211Deauthentication();
 
     bool getDeauthenticationPacket(char* packet);
