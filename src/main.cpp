e565c837eb5d509de17250e42b709b02e0047473
arad4228 <arad4228@gmail.com>
2023-01-26 16:16:47 +0900

    [KyoungJae]Add Init main code

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <pcap.h>
+
+void usage()
+{
+    std::cerr << "syntax : deauth-attack <interface> <ap mac> [<station mac> [-auth]]" << std::endl;
+    std::cerr << "sample : deauth-attack mon0 00:11:22:33:44:55 66:77:88:99:AA:BB" << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    if(argc < 3)
+    {
+        usage();
+        return -1;
+    }
+
+
+    return 0;
+}
